12d5c957261ef125a84cc9052161a66c11de42d3
Muhammed-Hasim <hasim.officehours@gmail.com>
2023-09-10 16:55:21 -0700

    added the solution to 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+
+/**
+ * main - entry point of the program
+ * Return: return 1
+ */
 int main(void)
 {
 	int i;
+
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		putchar(str[i]);
@@ -11,3 +18,4 @@ int main(void)
 	putchar('\n');
 	return (1);
 }
+
