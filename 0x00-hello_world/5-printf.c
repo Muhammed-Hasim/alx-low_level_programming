c9622a54aac3fd9dd3d47eb4d17bef04e038d077
Muhammed-Hasim <hasim.officehours@gmail.com>
2023-09-10 16:21:40 -0700

    added the solution to 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ * Return: return zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
