3dfc7d5d57516420391dcff40e4939e3f1c366e1
Muhammed-Hasim <hasim.officehours@gmail.com>
2023-09-10 16:12:22 -0700

    Aded the solution to problem set 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - the entry of the program
+ * Return: return zero
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
