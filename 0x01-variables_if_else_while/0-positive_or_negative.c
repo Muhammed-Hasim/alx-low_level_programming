d9581f040fb77d93eefae1a9fc2494591e7af7b5
Muhammed-Hasim <hasim.officehours@gmail.com>
2023-09-10 17:14:06 -0700

    added the solution to 0-positive_or_negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+		printf("is positive\n");
+	if (n == 0)
+		printf("is zero\n");
+	if (n < 0)
+		printf("is negative\n");
+
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This will be populated insha Allah by me it will contain the detail of all the projecs
